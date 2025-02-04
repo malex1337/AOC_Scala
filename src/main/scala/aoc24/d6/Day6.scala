34858da6d88ebff7c6aab1c63458235c21204752
alexander.mayr <alexander.mayr@dynatrace.com>
2025-02-04 14:01:49 +0100

    Day 6

--- src/main/scala/aoc24/d6/Day6.scala
+++ src/main/scala/aoc24/d6/Day6.scala
@@ -6,6 +6,7 @@ import scala.annotation.tailrec
 
 
 class Day6(fileName: String) extends Day(fileName) {
+  var visitedPositions = 0
 
   override protected def part1(): Unit = {
     val grid = lines.map(l => l.toCharArray).toArray
@@ -22,12 +23,48 @@ class Day6(fileName: String) extends Day(fileName) {
   }
 
   override protected def part2(): Unit = {
+    var amountLoopingPositions = 0
+    val grid = lines.map(l => l.toCharArray).toArray
 
+    val possibleObstacles =
+      for
+        row <- grid.indices
+        col <- grid(row).indices
+        if grid(row)(col) != '^'
+      yield Pos(col, row)
+
+    val guardPosition = grid.zipWithIndex.flatMap { case (row, rowIndex) =>
+      row.zipWithIndex.collect {
+        case (char, colIndex) if char == '^' => Pos(colIndex, rowIndex)
+      }
+    }.head
+    grid(guardPosition.y)(guardPosition.x) = '.'
+
+    for (possibleObstacle <- possibleObstacles) {
+      val gridCopy = grid.map(_.clone())
+      gridCopy(possibleObstacle.y)(possibleObstacle.x) = '#'
+      visitedPositions = 0
+      try {
+        walk(gridCopy, Pos(guardPosition.x, guardPosition.y), Direction(), Set(guardPosition))
+      }catch
+        case e: LoopingException =>
+      if(visitedPositions >= grid.length * grid(0).length + 1)
+        amountLoopingPositions+=1
+
+      visitedPositions = 0
+    }
+
+    println(s"Amnt of looping positions: $amountLoopingPositions")
   }
 
   @tailrec private def walk(grid: Array[Array[Char]], pos: Pos, dir: Direction, visited: Set[Pos]): Set[Pos] = {
+    if(visitedPositions >= grid.length * grid(0).length + 1)
+      throw new LoopingException
+
     val updated = if grid(pos.y)(pos.x) == '.' then visited + pos else visited
     val newPos = pos.walk(dir)
+
+    visitedPositions += 1
 
     charAt(grid, newPos) match
       case Some('.') => walk(grid, newPos, dir, updated)
@@ -53,6 +90,9 @@ class Day6(fileName: String) extends Day(fileName) {
   case class Pos(var x: Int, var y: Int) {
     def walk(direction: Direction): Pos = Pos(x + direction.x, y + direction.y)
   }
+
+  class LoopingException() extends Exception()
+
 }
 
 object Day6 {
